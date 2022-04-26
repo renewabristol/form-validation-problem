82bb2c910d06216deaa908c2e7c983c6709d9de1
Tom Lawson <thomas.a.sain@gmail.com>
2022-04-26 11:06:43 +0100

    new custom hook based on react state. returns: -change handler -submission handler -error and state data structurs

---
+++ form-test-tom/src/useForm.js
@@ -0,0 +1,77 @@
+import {useState} from 'react'
+
+export const useForm = (options) => {
+  const [data, setData] = useState((options?.initialValues || {}));
+  const [errors, setErrors] = useState({});
+
+  const handleChange = key => e => {
+    console.log(e.target.value)
+    console.log(key)
+    const value = e.target.type === 'checkbox'
+      ? e.target.checked 
+      : e.target.value
+    
+    if (key === 'animals') {
+      setData({
+        ...data,
+        animals: {
+          ...data.animals,
+          [e.target.value]:value
+        }
+      })
+    } else {
+
+      setData({
+        ...data,
+        [key]: value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validations = options?.validations;
+    if (validations) {
+      let valid = true;
+      const newErrors = {};
+      for (const key in validations) {
+        const value = data[key];
+        const validation = validations[key];
+        if (validation?.required?.value && !value) {
+          valid = false;
+          newErrors[key] = validation?.required?.message;
+        }
+
+        const pattern = validation?.pattern;
+        if (pattern?.value && !RegExp(pattern.value).test(value)) {
+          valid = false;
+          newErrors[key] = pattern.message;
+        }
+
+        const custom = validation?.custom;
+        if (custom?.isValid && !custom.isValid(value)) {
+          valid = false;
+          newErrors[key] = custom.message;
+        }
+      }
+
+      if (!valid) {
+        setErrors(newErrors);
+        return;
+      }
+    }
+
+    setErrors({});
+
+    if (options?.onSubmit) {
+      options.onSubmit();
+    }
+  };
+
+  return {
+    data,
+    handleChange,
+    handleSubmit,
+    errors,
+  };
+};
